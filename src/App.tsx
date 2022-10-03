a35fa19aeb0e58d77f879cf847e562eb8b329873
Dharuan Luigi <dharuanluigi@gmail.com>
2022-10-03 15:39:14 -0300

    Finish theory classes and eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
+
+import { defaultTheme } from './styles/default'
+import { GlobalStyle } from './styles/global'
+
 export function App() {
-  return <div className="App">Hi!</div>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
